52687f25a29a73ab08d306f020ff829777250c32
yanchenhugh <ychu.span.osu@gmail.com>
2024-12-22 20:56:35 -0500

    add new NLP projects

--- projects/.ipynb_checkpoints/utils_projects-checkpoint.py
+++ projects/.ipynb_checkpoints/utils_projects-checkpoint.py
@@ -3,7 +3,7 @@
 
 # In[ ]:
 
-from collections import Counter
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -111,43 +111,3 @@ def imshow(image, ax=None, title=None, normalize=True):
     ax.set_yticklabels('')
 
     return ax
-
-def preprocess(text):
-
-    # Replace punctuation with tokens so we can use them in our model
-    text = text.lower()
-    text = text.replace('.', ' <PERIOD> ')
-    text = text.replace(',', ' <COMMA> ')
-    text = text.replace('"', ' <QUOTATION_MARK> ')
-    text = text.replace(';', ' <SEMICOLON> ')
-    text = text.replace('!', ' <EXCLAMATION_MARK> ')
-    text = text.replace('?', ' <QUESTION_MARK> ')
-    text = text.replace('(', ' <LEFT_PAREN> ')
-    text = text.replace(')', ' <RIGHT_PAREN> ')
-    text = text.replace('--', ' <HYPHENS> ')
-    text = text.replace('?', ' <QUESTION_MARK> ')
-    # text = text.replace('\n', ' <NEW_LINE> ')
-    text = text.replace(':', ' <COLON> ')
-    words = text.split()
-    
-    # Remove all words with  5 or fewer occurences
-    word_counts = Counter(words)
-    trimmed_words = [word for word in words if word_counts[word] > 5]
-
-    return trimmed_words
-
-
-def create_lookup_tables(words):
-    """
-    Create lookup tables for vocabulary
-    :param words: Input list of words
-    :return: A tuple of dicts.  The first dict....
-    """
-    word_counts = Counter(words)
-    # sorting the words from most to least frequent in text occurrence
-    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
-    # create int_to_vocab dictionaries
-    int_to_vocab = {ii: word for ii, word in enumerate(sorted_vocab)}
-    vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
-
-    return vocab_to_int, int_to_vocab
